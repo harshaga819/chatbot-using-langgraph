25d9fcea6aea9f597e79782e1ab2ee68acd0a8b1
Harsh Agarwal <harshaga819@gmail.com>
2026-07-18 18:10:37 +0530

    Small changes in config.py file

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
 from langchain_groq import ChatGroq
-from pathlib import Path
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
-# Project Paths
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-CHROMA_DB_PATH = BASE_DIR / "data" / "chroma_db"
-
-# Model Configuration
 
 LLM_MODEL = "llama-3.3-70b-versatile"
 
@@ -22,8 +13,4 @@ embeddings = HuggingFaceEndpointEmbeddings(
     huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
 )
 
-
-llm = ChatGroq(
-    model=LLM_MODEL,
-    temperature= 0.3
-)
+llm = ChatGroq(model=LLM_MODEL, temperature= 0.3)
